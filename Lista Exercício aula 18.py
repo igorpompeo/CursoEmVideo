8a19ed247fa2105e4ee57e24cf7bd73e6abe3aee
Igor Pompeo <pompbass@gmail.com>
2019-04-25 14:58:01 -0300

    Adicionado do repo local

---
+++ Lista Exercício aula 18.py
@@ -0,0 +1,27 @@
+'''
+Exercício 84 - Faça um programa que leia nome e peso de várias pessoas,
+guardando tudo em uma lista. No final mostre:
+A) Quantas pessoas foram cadastradas;
+B) Uma listagem com as pessoas mais pesadas;
+C) Uma listagem com as pessoas mais leves;
+
+Exercício 85 - Crie um programa onde o usuário posso digitar sete valores
+numéricos e cadastre-os em uma lista única que mantenha separados os valores
+pares e impares. No final, mostre os valores pares e ímpares em ordem crescente.
+
+Exercício 86 - Crie um programa que crie uma matriz de dimensão 3x3 e preencha com
+valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
+
+Exercício 87 - Aprimore o desafio anterior, mostrando no final:
+A) a soma de todos os valores pares digitados.
+B) a soma dos valores da terceira coluna.
+C) o maior valor da segunda linda.
+
+Exercício 88 - Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
+O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60
+para cada jogo, cadastrando tudo em uma lista composta.
+
+Exercício 89 - Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta.
+No final, mostre o boletim contendo a média de cada um e permita que o usuário posso mostrar as notas de cada aluno
+individualmente.
+'''
---
+++ Lista Exercício aula 19.py
@@ -0,0 +1,33 @@
+'''
+Exercício 90
+Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+No final, mostre o conteúdo da estrutura na tela.
+
+Exercício 91
+Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios.
+Guarde esses resultados em um dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor
+tirou o maior número no dado.
+
+Exercício 92
+Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os(com idade) em um dicionário
+se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e
+acrescente, além da idade, com qunatos anos a pessoa vai se aposentar.
+
+Exercício 93
+Crie um programa que gerencie o aproveitamento de um jogador de futebol.
+O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos
+em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos
+durante o campeonato.
+
+Exercício 94
+Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada dicinoário e todos os
+dicionários em uma lista. No final, mostre:
+A) quantas pessoas foram cadastradas
+B) a média de idade do grupo
+C) uma lista com todas as mulheres
+D) uma lista com todas as pessoas com idade acima da média.
+
+Exercício 95
+Aprimore o Exercício 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes de
+aproveitamento de cada jogador.
+'''
