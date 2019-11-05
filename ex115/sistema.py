aaf6e6d8d2abca9a06e9ab4311188fa814d9fb81
igorpompeo <igor.pompeo1@ibm.com>
2019-11-05 17:20:41 -0300

    Exercício 115, visto o vídeo da aula A e B.

---
+++ ex115/sistema.py
@@ -0,0 +1,22 @@
+from CursoEmVideo.ex115.lib.interface import *
+from CursoEmVideo.ex115.lib.arquivo import *
+from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastra novas Pessoas', 'Sair do sistema'])
+    if resposta == 1:
+        # Opção de listar o conteúdo de um arquivo
+        lerArquivo(arq)
+    elif resposta == 2:
+        cabeçalho('Opção 2')
+    elif resposta == 3:
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
