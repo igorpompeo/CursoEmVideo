a5a45bb2a9c0eed37ca129ba9685189f56979af6
igorpompeo <igor.pompeo1@ibm.com>
2019-11-18 10:26:40 -0300

    Exercício 115, visto o vídeo da aula A, B e C.

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -13,10 +13,16 @@ while True:
         # Opção de listar o conteúdo de um arquivo
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        # Opção de cadastra uma nova pessoa.
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = int(input('Idade: '))
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema.
         cabeçalho('Saindo do sistema... Até logo!')
         break
     else:
+        # Digitou uma opção errado no menu.
         print('\033[31mERRO! Digite uma opção válida!\033[m')
     sleep(2)
