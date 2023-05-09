b0b8ea3349c4da4c8f6b0cded30c11b739e0ca7a
Igor Pompeo <igor.pompeo1@ibm.com>
2023-05-09 20:07:18 -0300

    Aula08 - Objetos compostos UltraEmojiCombat - UEC

---
+++ POO/JAVA/Aula07/src/Luta.java
@@ -0,0 +1,16 @@
+/**
+ * Classe agregada de Lutador
+ * @author 069547631
+ * @version 1.00
+ * @since 09 - maio - 2023
+ */
+public class Luta {
+    // Atributos
+    private String desafiado, desafiante;
+    private int rounds;
+    private boolean aprovada;
+
+    // Métodos públicos
+    public void marcarLuta(){}
+    public void lutar(){}
+}
